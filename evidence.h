04b3c25cdfb0b975ffd57769b6635a5f54a1879e
Darrion Singh <1056673@students.wits.ac.za>
2019-03-23 19:24:20 +0200

    Added evidence factory and implementation. Still need to inject into council

--- evidence.h
+++ evidence.h
@@ -3,19 +3,16 @@
 
 #include <string>
 #include <fruit/fruit.h>
-#include <iostream>
 
-struct Evidence
+class Evidence
 {
-  public:
-    INJECT(Evidence(ASSISTED(std::string) name)) : name_(name) {}
-    void name()
-    {
-        std::cout << name_;
-    }
-
-  private:
-    std::string name_;
+public:
+  virtual void name() = 0;
+  virtual ~Evidence() = default;
 };
+
+using EvidenceFactory = std::function<std::unique_ptr<Evidence>(std::string)>;
+
+fruit::Component<EvidenceFactory> getEvidenceComponent();
 
 #endif
