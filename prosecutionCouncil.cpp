ddf10e5ac349be3b459a50963f924683d77db46e
Darrion Singh <1056673@students.wits.ac.za>
2019-03-23 18:31:05 +0200

    added functionality to prosecution evidence still not injected

--- prosecutionCouncil.cpp
+++ prosecutionCouncil.cpp
@@ -1,36 +1,53 @@
 #include "prosecutionCouncil.h"
 #include "complainantWitness.h"
 #include "evidence.h"
+#include <iostream>
 
 class ProsecutionCouncilImpl : public Council
 {
 private:
   Witness *complainant_;
   Evidence *weapon_;
+  bool calledComplainant_;
 
 public:
-  INJECT(ProsecutionCouncilImpl(ANNOTATED(ComplainantWitness, Witness *) complainant)) : complainant_(complainant)
+  INJECT(ProsecutionCouncilImpl(ANNOTATED(ComplainantWitness, Witness *) complainant)) : complainant_(complainant), calledComplainant_(false)
   {
   }
 
   virtual void presentEvidence() override
   {
+    std::cout << "The Prosecution presents a ";
+    weapon_->name();
+    std::cout << "into evidence." << std::endl;
   }
 
   virtual void crossExamineWitness() override
   {
+    std::cout << "The Prosecution is Cross-Examining the witness." << std::endl;
   }
 
   virtual void callWitness() override
   {
+    if (!calledComplainant_)
+    {
+      std::cout << "The Prosecution calls Complainant to the stand." << std::endl;
+      !calledComplainant_;
+    }
+    else
+    {
+      std::cout << "The Prosecution has no more witnesses to call." << std::endl;
+    }
   }
 
   virtual void objection() override
   {
+    std::cout << "The Prosecution objects to the Defense's statements." << std::endl;
   }
 
   virtual void leadWitness() override
   {
+    std::cout << "The Prosecution leads the complainant." << std::endl;
   }
 };
 
