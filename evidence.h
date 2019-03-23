ddb4a97fdf1eb97eb7d87d36a80d1a61ade6d5b4
Darrion Singh <1056673@students.wits.ac.za>
2019-03-23 16:04:14 +0200

    added display name function to evidence

--- evidence.h
+++ evidence.h
@@ -3,11 +3,16 @@
 
 #include <string>
 #include <fruit/fruit.h>
+#include <iostream>
 
 struct Evidence
 {
   public:
     INJECT(Evidence(ASSISTED(std::string) name)) : name_(name) {}
+    void name()
+    {
+        std::cout << name_ << std::endl;
+    }
 
   private:
     std::string name_;
