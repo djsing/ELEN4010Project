cd3e6d1febd7d10cc3c2d76a81007ce2d6f6e49c
Darrion Singh <1056673@students.wits.ac.za>
2019-03-24 15:11:28 +0200

    court header file

---
+++ court.h
@@ -0,0 +1,16 @@
+#ifndef COURT_H
+#define COURT_H
+
+#include <fruit/fruit.h>
+
+class Court
+{
+  public:
+    virtual void begin() = 0;
+    virtual void recess() = 0;
+    virtual void adjourn() = 0;
+};
+
+fruit::Component<Court> getCourtComponent();
+
+#endif
