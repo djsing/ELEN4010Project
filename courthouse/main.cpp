6f35a482216a261d04f92ce1818f2b3630f099d3
Darrion Singh <1056673@students.wits.ac.za>
2019-04-04 20:08:29 +0200

    removed courthouse

--- courthouse/main.cpp
+++ courthouse/main.cpp
@@ -1,29 +1,9 @@
 #include <fruit/fruit.h>
-#include "courthouse.h"
 #include "criminalCourt.h"
 #include <iostream>
 
 int main()
 {
     std::cout << "Welcome to the Magistrate's Court." << std::endl;
-    std::cout << "Enter 1 to enter the Criminal Court, 2 to enter the Civil Court, and any other key to exit." << std::endl;
-    int choice;
-    std::cin >> choice;
-    std::cout << std::endl;
-
-    //fruit::Injector<fruit::Required<fruit::Component<fruit::Annotated<CriminalCourt, Court>>>, CourtHouse> injector(getCourtHouseComponent);
-
-    switch (choice)
-    {
-    case 1:
-        std::cout << "Criminal Court" << std::endl;
-        break;
-
-    case 2:
-        std::cout << "Civil Court" << std::endl;
-        break;
-
-    default:
-        break;
-    }
+    fruit::Injector<fruit::Annotated<CriminalCourt, Court>> criminalCourtInjector(getCriminalCourtComponent);
 }
